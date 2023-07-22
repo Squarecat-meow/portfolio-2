b819b7c9b5dd0256ebace5d7304c879b72d1311a
Squarecat-meow <kw7551@gmail.com>
2023-07-23 01:53:47 +0900

    add alert message on sign out button

--- src/components/Main/Setting/SettingModal/UserSetting.jsx
+++ src/components/Main/Setting/SettingModal/UserSetting.jsx
@@ -1,9 +1,12 @@
-import { Button } from "antd";
+import { Button, Popconfirm } from "antd";
 import React from "react";
 
 import { useDispatch } from "react-redux";
 import { upLogout } from "../../../../redux/slices/LoginSlices";
 import { useNavigate } from "react-router-dom";
+
+import { deleteUser } from "firebase/auth";
+import { auth } from "../../../../config/firebase";
 
 const UserSetting = () => {
   const dispatch = useDispatch();
@@ -14,11 +17,31 @@ const UserSetting = () => {
     navigate("/");
   };
 
+  const handleSignout = () => {
+    const user = auth.currentUser;
+    deleteUser(user)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   return (
-    <div>
-      <Button type="primary" danger onClick={handleLogout}>
+    <div className="grid grid-flow-col gap-4">
+      <Button type="default" danger onClick={handleLogout}>
         Log out
       </Button>
+      <Popconfirm
+        title="Delete account"
+        description="Are you sure to delete this account?"
+        onConfirm={handleSignout}
+      >
+        <Button type="primary" danger>
+          Sign out
+        </Button>
+      </Popconfirm>
     </div>
   );
 };
